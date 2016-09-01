358b710c02c716934e174db1c553550b5a6e1e3a
pakss3 <pakss3@naver.com>
2016-09-01 18:34:12 +0900

    hello world console 작성

---
+++ helloworld/src/kr/co/saramin/helloworld/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package kr.co.saramin.helloworld.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("helloworldddddddd");
+	}
+
+}
